429dd5b68d86ba10f2af3ffdff534f55885672a8
ST10451846 <161336379+ST10451846@users.noreply.github.com>
2025-03-05 16:41:42 +0200

    Add project files.

---
+++ PolySimple05/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolySimple05 // Abstract classes allow us to enforce method
+                       // implementation in derived classes
+{
+    abstract class Vehicle
+    {
+        public abstract void Start();
+    }
+
+    class Car: Vehicle
+    {
+        public override void Start()
+        {
+            Console.WriteLine("Car is starting.....");
+        }
+    }
+
+    class Bike: Vehicle
+    {
+        public override void Start()
+        {
+            Console.WriteLine("Bike is starting...");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicle v1 = new Car();
+            Vehicle v2 = new Bike();
+
+            v1.Start(); // Output: Car is starting......
+            v2.Start(); // Output: Bike is starting......
+        }
+    }
+}
